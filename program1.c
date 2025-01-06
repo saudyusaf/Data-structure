654291e28a0294d14299fd0e7584d7cf1f803104
saudyusaf <saudyusaf289@gmail.com>
2025-01-06 09:56:30 +0530

    Add files via upload
    my second commit

---
+++ program1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+printf("enter two number:");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum+%d",sum);
+}
---
+++ program2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int myfunction(intx,inty)
+{
+int c;
+c=x+y;
+return(c);
+}
+int main()
+{
+int a,b;
+printf("enter two numbers:");
+scanf("%d%d",&a,&b);
+printf("result is=%d",result);
+return 0;
+}
